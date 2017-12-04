411bef60d280b65ed4c7c0b1ee06789c667f0964
Bob Wilson <millstadtf@gmail.com>
2017-12-03 17:06:04 -0800

    Adding MANIFEST file to include unit definition file. Adding some description to the setup.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
 setup(name='yaucl',
-      version='0.0.1',
+      version='0.1',
       description='Yet Another Unit Conversion Library',
+      keywords=['unit', 'conversion', 'parsing',
+                'physics', 'engineering'],
       url='https://github.com/rwilson4/yaucl',
       author='Bob Wilson',
       author_email='bob@causalityinc.com',
-      licence='Apache',
+      licence='Apache 2.0',
       packages=['yaucl'],
+      include_package_data=True,
+      long_description=open('README.md').read(),
       zip_safe=False)
