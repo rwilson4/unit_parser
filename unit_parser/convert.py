50e85269d8a2460fac258cf14e114087417dd92b
Bob Wilson <millstadtf@gmail.com>
2017-12-21 20:25:03 -0800

    Adding license info to all major fails and updating email address.

--- unit_parser/convert.py
+++ unit_parser/convert.py
@@ -1,7 +1,24 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
 from unit_parser import unit_parser
+
+#   File: convert.py
+#   Purpose: Wrapper for command line utility for converting between
+#   units.
+#
+#   Copyright 2017 Bob Wilson
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 
 def main():
     import argparse
